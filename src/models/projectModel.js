1d89183ce2bd8b9e6b0ce635474b975ed2f68b3c
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-12 22:29:11 +0000

    Add project schema

--- src/models/projectModel.js
+++ src/models/projectModel.js
@@ -1,1 +1,29 @@
 import mongoose from 'mongoose';
+
+const projectSchema = new mongoose.Schema({
+  img_src: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  git_link: {
+    type: String
+  },
+  deploy_link: {
+    type: String
+  },
+  short_desc: {
+    type: String,
+    required: true
+  },
+  article: {
+    type: String
+  }
+});
+
+const project = mongoose.model('project', projectSchema);
+
+export default project;
