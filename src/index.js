59194027b7d90be0f3dfc1f3291da71fefcb5e24
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-12 22:02:54 +0000

    Add test data

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
+
+// Test data
+const projects = [
+  {
+    "img_src": "https://imgur.com/NjLjiJS.png",
+    "title": "Sleep Tracker",
+    "git_link": "https://github.com/build-week-sleep-tracker",
+    "deploy_link": "https://bw-sleep-tracker-fe.netlify.com/",
+    "short_desc": "Sleep Tracker is an app enabling users to track their sleep and receive feedback and recommendations on their sleep patterns. This was built by 6 developers in 1 week"
+  },
+  {
+    "img_src": "https://imgur.com/nCcmSTl.png",
+    "title": "Wunderlist 2.0",
+    "git_link": "https://github.com/team-wunderlist",
+    "short_desc": "This was my first build week at Lambda School. We were tasked with creating a list-keeping app to help manage people tasks throughout the day. I created the landing page and part of the React app"
+  }
+];
