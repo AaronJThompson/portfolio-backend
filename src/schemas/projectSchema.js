b9fad23894e88376453ae764eb1eba013f69c0a0
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-12 22:34:53 +0000

    Create project schema

---
+++ src/schemas/projectSchema.js
@@ -0,0 +1,18 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+  type Project {
+    id: ID!
+    title: String!
+    img_src: String!
+    git_link: String
+    deploy_link: String
+    short_desc: String!
+    article: String
+  }
+
+  extend type Query {
+    project(title: String!): Project
+    projects: [Project]
+  }
+`
